2fc5f86a286890933ec0b1df6054e240a199dfb7
DavidEdwardDarden <davidedarden83@gmail.com>
2021-05-27 12:50:47 -0500

    finiished through wish list

---
+++ insulter/csharp/insulter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace insulter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Insults = new List<string>{
+            "You look like what morning breath smells like.",
+            "If you tried to give me cpr I would probably throw myself back under water",
+            "I am not a fan of you",
+            "You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
+           "I'd rather walk than be on the same plane as you",
+            "If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
+            "I am not a fan of you"
+            };
+
+            List<int> Indexed = new List<int>();
+           
+            Random r = new Random();
+
+            while (Indexed.Count < 3)
+            {
+                int candidate = r.Next(0, Insults.Count - 1);
+                if (!Indexed.Contains(candidate))
+                {
+                    Indexed.Add(candidate);
+                }
+            }
+
+            for (int i = 0; i < Indexed.Count; i++)
+            {
+                int index = Indexed[i];
+                Console.Write(Insults[index]);
+            }
+
+
+
+        }
+    }
+}
---
+++ wish-list/csharp/wish-list/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wish_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> WishList = new List<string>();
+           WishList.Add("An original eight track of William Shatner's 'Lucy in the Sky with Diamonds'");    
+          WishList.Add("The ability to think of interesting wishes on short notice");   
+           WishList.Add("A washtub of Skittles");   
+           WishList.Add("World peace, but not the kind of world peace where all the humans are removed from existence");  
+            WishList.Add("A TV that is NOT internet-capable"); 
+             WishList.Add("The ability to fly, but, like, really fast...not just, like, how fast I can walk, but, like, fast, you know?"); 
+             
+             Console.WriteLine("My wishes...");
+             
+             foreach (string wish in WishList){
+                  Console.WriteLine(wish);
+             }
+
+        }
+
+    }
+}
